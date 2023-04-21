1d32b065763ac5b946759553aaa963dcbf26cfa6
Harikrishnan <harikrishnanb24@gmail.com>
2023-04-22 00:29:06 +0530

    bug

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+TweenMax.staggerFrom(".block",0.8,{
+    width: "0%",
+    ease: Power1.easeIn,
+    delay: 2,
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+},0.04);
 
-setupCounter(document.querySelector('#counter'))
+TweenMax.to(".loader",1,{
+    x:2,
+    opacity:0,
+    ease:Expo.easeInOut,
+    delay:1.5
+
+})
+
+TweenMax.staggerFrom(".nav > a, .about p, .sub-header > a" , 2,{
+    opacity:0,
+    y:30,
+    ease: Expo.easeInOut,
+    delay:3
+},0.06)
+
+TweenMax.to(".box",0.2,{
+    opacity:1,
+    ease:Expo.easeInOut,
+    delay: 3.8
+})
+
+TweenMax.to(".img",0.2,{
+    opacity:1,
+    ease:Expo.easeInOut,
+    delay: 4
+})    
+
+TweenMax.to(".box",2.4,{
+    y:"-100%",
+    ease:Expo.easeInOut,
+    delay: 4
+})
