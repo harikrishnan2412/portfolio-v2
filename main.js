a3b48c4bf7f4b3474787020197d94325943d7c16
Joel K George <joelk9895@gmail.com>
2023-04-22 00:42:59 +0530

    fixed the bug ditch 🥲

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
-TweenMax.staggerFrom(".block",0.8,{
+TweenMax.staggerFrom(
+  ".block",
+  0.8,
+  {
     width: "0%",
     ease: Power1.easeIn,
     delay: 2,
+  },
+  0.04
+);
 
-},0.04);
+TweenMax.to(".loader", 1, {
+  x: 2,
+  opacity: 0,
+  ease: Expo.easeInOut,
+  delay: 1.5,
+});
 
-TweenMax.to(".loader",1,{
-    x:2,
-    opacity:0,
-    ease:Expo.easeInOut,
-    delay:1.5
-
-})
-
-TweenMax.staggerFrom(".nav > a, .about p, .sub-header > a" , 2,{
-    opacity:0,
-    y:30,
+TweenMax.staggerFrom(
+  ".nav > a, .about p, .sub-header > a",
+  2,
+  {
+    opacity: 0,
+    y: 30,
     ease: Expo.easeInOut,
-    delay:3
-},0.06)
+    delay: 3,
+  },
+  0.06
+);
 
-TweenMax.to(".box",0.2,{
-    opacity:1,
-    ease:Expo.easeInOut,
-    delay: 3.8
-})
+TweenMax.to(".box", 0.2, {
+  opacity: 1,
+  ease: Expo.easeInOut,
+  delay: 3.8,
+});
 
-TweenMax.to(".img",0.2,{
-    opacity:1,
-    ease:Expo.easeInOut,
-    delay: 4
-})    
+TweenMax.to(".image", 0.2, {
+  opacity: 1,
+  ease: Expo.easeInOut,
+  delay: 4,
+});
 
-TweenMax.to(".box",2.4,{
-    y:"-100%",
-    ease:Expo.easeInOut,
-    delay: 4
-})
+TweenMax.to(".box", 2.4, {
+  y: "-100%",
+  ease: Expo.easeInOut,
+  delay: 4,
+});
